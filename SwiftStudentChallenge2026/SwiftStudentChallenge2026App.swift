38a34af481b8e92317c74d386ea3f8d5f3b415a6
Ankit Bansal <bansalankit1575@gmail.com>
2026-02-10 12:14:37 +0530

    Initial Commit

---
+++ SwiftStudentChallenge2026/SwiftStudentChallenge2026App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStudentChallenge2026App.swift
+//  SwiftStudentChallenge2026
+//
+//  Created by Ankit bansal on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStudentChallenge2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
